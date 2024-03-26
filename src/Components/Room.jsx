bb9121278fcbb168b5f4ea9e5baa25b21bb7b9da
anantnipunge <anantnipunge425@gmail.com>
2024-03-26 22:37:40 +0530

    initial commit

---
+++ src/Components/Room.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt'
+
+const Room = () => {
+    const { roomID } = useParams();
+
+    const mymeeting = async (element) => {
+        const appId = 1671577894;
+        const serverSecret = "9bb15bf2bcb89875bc0709697c18a40a";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appId, serverSecret, roomID, Date.now().toString(), "Enter Your Name");
+
+        const zc = ZegoUIKitPrebuilt.create(kitToken);
+
+        zc.joinRoom({
+            container: element,
+            sharedLinks: [{
+                name: "Copy Link",
+                // url: `https://refmemeet.vercel.app//room/${roomID}`
+                url: `localhost:8080/room/${roomID}`
+            }],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.OneONoneCall,
+            },
+
+        })
+    }
+
+    return (
+        <div><div ref={mymeeting} /></div>
+    )
+}
+
+export default Room
